579b76a5ee975321dbfa275a938ca63c9249e126
Horacio Valdes <horavaldesg@gmail.com>
2022-02-12 12:25:09 -0500

    Two Numbers Adding

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter Two Number: ");
+		System.out.print("> ");
+		int firstNumber = kb.nextInt();
+
+		System.out.print("> ");
+		int secondNumber = kb.nextInt();
+
+		int result = firstNumber + secondNumber;
+
+		System.out.println(firstNumber + " + " + secondNumber + " = "+result);
 	}
 }
